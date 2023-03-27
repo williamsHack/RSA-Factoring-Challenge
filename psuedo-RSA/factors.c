fe51853b91f855791c3b11c44398d8efd53adc79
williamsHack <williamyawdickson1998@gmail.com>
2023-03-27 07:49:46 -0700

    New changes

--- psuedo-RSA/factors.c
+++ psuedo-RSA/factors.c
@@ -1,46 +1,33 @@
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <math.h>
-#include <string.h>
+#include "factor.h"
 
+/**
+ * main - main function
+ *
+ *
+ * Return: void
+ */
 int main(int argc, char *argv[])
 {
-	FILE *stream;
-	char *line = NULL;
-	size_t len = 0;
-	long long flag = 1, div, rest, number, counter;
-	ssize_t nread;
+	FILE *fptr;
+	size_t count;
+	ssize_t line;
+	char *buffer = NULL;
 
-	if (argc != 2) {
-		fprintf(stderr, "Usage: %s <file>\n", argv[0]);
+
+	if (argc != 2)
+	{
+		fprintf(stderr, "Usage: factor <filename>\n");
 		exit(EXIT_FAILURE);
 	}
-
-	stream = fopen(argv[1], "r");
-	if (stream == NULL) {
-		perror("fopen");
+	fptr = fopen(argv[1], "r");
+	if (fptr == NULL)
+	{
+		fprintf(stderr, "Error: can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
-
-	while ((nread = getline(&line, &len, stream)) != -1) {
-		flag = 1, div = 2;
-		number = atoll(line);
-		while (flag) {
-			rest = number % div;
-			if (!rest) {
-				counter = number / div;
-				printf("%lld=%lld*%lld\n", number, counter, div);
-				flag = 0;
-			}
-			div++;
-		}
+	while((line = getline(&buffer, &count, fptr)) != -1)
+	{
+		factorize(buffer);
 	}
-
-	free(line);
-	fclose(stream);
-	exit(EXIT_SUCCESS);
+return (0);
 }
